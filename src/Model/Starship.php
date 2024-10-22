56e37dcf23467bba4744956d9a23d83597892577
Dzhemile-dzh <dzhemile_ahmet@abv.bg>
2024-10-22 13:08:30 +0300

    added model

---
+++ src/Model/Starship.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Model;
+
+class Starship
+{
+    public function __construct(
+        private int $id,
+        private string $name,
+        private string $class,
+        private string $captain,
+        private string $status,
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    public function setClass(string $class): self
+    {
+        $this->class = $class;
+
+        return $this;
+    }
+
+    public function getCaptain(): string
+    {
+        return $this->captain;
+    }
+
+    public function setCaptain(string $captain): self
+    {
+        $this->captain = $captain;
+
+        return $this;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
